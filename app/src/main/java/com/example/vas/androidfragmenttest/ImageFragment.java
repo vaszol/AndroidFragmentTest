d894a29a10454c77f3c6ca08e44c64b209a30786
vaszol <vaszol@mail.ru>
2014-12-22 08:45:45 +0300

    Initial commit

---
+++ app/src/main/java/com/example/vas/androidfragmenttest/ImageFragment.java
@@ -0,0 +1,19 @@
+package com.example.vas.androidfragmenttest;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by vas on 22-Dec-14.
+ */
+public class ImageFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.image_layout, container,false);
+//        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
---
+++ app/src/main/java/com/example/vas/androidfragmenttest/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.vas.androidfragmenttest;
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+
+public class MainActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+
+}
